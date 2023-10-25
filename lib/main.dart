eba3856fa2b24cb4e7c293c53139a97b92f2c2f8
yeodae <happy940823@naver.com>
2023-10-25 11:21:30 +0900

    인류 최초 두번째 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
                 children: [
                   TabBar(
                     tabs: [
-                      Tab(text: 'Tab 1'),
+                      Tab(text: 'NOW'),
                       Tab(text: 'Tab 2'),
                     ],
                   ),
