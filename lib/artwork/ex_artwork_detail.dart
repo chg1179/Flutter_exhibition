1e988fbfa304210dbdf28ecb6efb7fafe4f5c5f2
jinaleee <zz60415@gmail.com>
2023-10-31 15:31:50 +0900

    작품 상세 완료우

--- lib/artwork/ex_artwork_detail.dart
+++ lib/artwork/ex_artwork_detail.dart
@@ -77,25 +77,28 @@ class _ExArtworkDetailState extends State<ExArtworkDetail> {
                             ],
                           ),
                           Text(_artworkInfo['art_material']),
+                          SizedBox(height: 5,),
                           Text("${_artworkInfo['art_size']} ${_artworkInfo['art_date']}"),
                         ],
                       ),
-                    ), // 상단 내용 패딩
+                    ),
+                    SizedBox(height: 20,),
                     Divider(
                       color: Color(0xff989898), // 선의 색상 변경 가능
                       thickness: 0.3, // 선의 두께 변경 가능
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(top: 15, bottom: 15),
+                      padding: const EdgeInsets.only(top: 10, bottom: 10),
                       child: InkWell(
                         child: Row(
                           children: [
+                            SizedBox(width: 10),
                             CircleAvatar(
                               radius: 30, // 반지름 크기 조절
-                              backgroundImage: AssetImage("assets/${widget.imagePath}"),
+                              backgroundImage: AssetImage("${widget.imagePath}"),
                             ),
-                            SizedBox(width: 8,),
-                            Text("차승언")
+                            SizedBox(width: 15),
+                            Text("차승언", style: TextStyle(fontSize: 16),)
                           ],
                         ),
                       )
@@ -104,23 +107,13 @@ class _ExArtworkDetailState extends State<ExArtworkDetail> {
                       color: Color(0xff989898), // 선의 색상 변경 가능
                       thickness: 0.3, // 선의 두께 변경 가능
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(15),
-                      child: Text("국립아시아문화전당은 전 세계인들이 공감할 수 있는 동시대 주요 주제와 이슈, 아시아를 비롯한 세계 역사와 문화에 대한 다학제적 연구와 창제작을 기반으로 한 다양한 전시를 선보이고 있습니다. 교류에서 시작하여 창조와 연구, 교육으로 직접 순환되는 아시아 문화의 발전소가 되어 다양한 시각에서 문화예술 콘텐츠를 생산하고, 이를 전시로 시민들에게 선보임으로써 함께 참여하고, 공감하는 자리를 마련하고자 합니다."),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 15),
-                      child: Divider(
-                        color: Color(0xff989898), // 선의 색상 변경 가능
-                        thickness: 0.3, // 선의 두께 변경 가능
-                      ),
-                    ),
+                    SizedBox(height: 10,),
                     Container(
                       height: 30,
                       child: Stack(
                         children: [
                           Text(
-                            "   진행 중 전시회",
+                            "   추천 작품",
                             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                           ),
                           Positioned(
@@ -137,31 +130,7 @@ class _ExArtworkDetailState extends State<ExArtworkDetail> {
                     ),
                     Container(
                       height: 200,
-                      child: Center(child: Text("진행 중 전시가 없습니다.")),
-                    ),
-                    Container(
-                      height: 30,
-                      child: Stack(
-                        children: [
-                          Text(
-                            "   예정 전시회",
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            left: 0,
-                            child: Container(
-                              height: 2.0, // 밑줄의 높이
-                              width: 140,
-                              color: Colors.black, // 밑줄의 색
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      height: 200,
-                      child: Center(child: Text("예정 전시가 없습니다.")),
+                      child: Center(child: Text("연관 작품이 없습니다.")),
                     ),
                   ]
               ),
