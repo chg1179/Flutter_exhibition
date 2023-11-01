c28122a884fb358285fdfbd937ddf667c689d19a
yeodae <happy940823@naver.com>
2023-11-02 01:04:42 +0900

    jtbi result page ~ ing

--- lib/myPage/JTBI/my_jtbi_result.dart
+++ lib/myPage/JTBI/my_jtbi_result.dart
@@ -39,6 +39,59 @@ class _JtbiResultState extends State<JtbiResult> {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // 텍스트 사이의 간격을 조절
+        children: [
+          Text(
+            '선호 키워드',
+            style: TextStyle(
+              color: Colors.black, // 검은색
+              fontSize: 18, // 글씨 크기
+              fontWeight: FontWeight.bold, // 굵게
+            ),
+          ),
+          Text(
+            '서정적',
+            style: TextStyle(
+              color: Colors.purple, // 보라색
+              fontSize: 18, // 글씨 크기
+              fontWeight: FontWeight.bold, // 굵게
+            ),
+          ),
+        ],
+      ),
+
+    );
+  }
+}
+class TemperatureBar extends StatelessWidget {
+  final double temperature;
+
+  TemperatureBar({required this.temperature});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 80),
+              child: Text('현재 온도: $temperature°C', style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ),
+        SizedBox(height: 10),
+        Container(
+          height: 10, // 온도바의 높이 조정
+          width: 350, // 온도바의 너비 조정
+          decoration: BoxDecoration(
+            color: Colors.green, // 온도바의 색상을 초록색으로 설정
+            borderRadius: BorderRadius.circular(10.0), // 둥근 모서리 설정
+          ),
+        ),
+      ],
     );
   }
 }
