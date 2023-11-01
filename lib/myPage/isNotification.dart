27b5b49119f602b7f317cd3d200c6231cffd28ef
jinaleee <zz60415@gmail.com>
2023-11-01 18:10:05 +0900

    알림 페이지 수정

--- lib/myPage/isNotification.dart
+++ lib/myPage/isNotification.dart
@@ -8,10 +8,23 @@ class IsNotification extends StatefulWidget {
 }
 
 class _IsNotificationState extends State<IsNotification> {
+  final List<Map<String, dynamic>> _notificationList = [
+    {'content': '단팥빵 님이 회원님의 게시글에 댓글을 달았습니다.', 'time': '5분 전'},
+    {'content': '크로와상 님이 회원님의 게시글에 댓글을 달았습니다.', 'time': '1시간 전'},
+    {'content': '촠호췹쿸키 님이 회원님을 팔로우하기 시작했습니다.', 'time': '어제'},
+    {'content': '참깨빵위에순쇠고기패티두장 님이 회원님을 팔로우하기 시작했습니다.', 'time': '어제'},
+    {'content': '"박영하" 작가의 전시가 등록되었습니다.', 'time': '2023.10.28'},
+    {'content': '[추천 전시] 미야자키 하이요 <<그대들은 어떻게 먹을 것인가>>', 'time': '2023.10.27'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+          "알림",
+          style: TextStyle(color: Colors.black, fontSize: 19),
+        ),
         backgroundColor: Colors.white,
         elevation: 1.0,
         leading: IconButton(
@@ -21,15 +34,82 @@ class _IsNotificationState extends State<IsNotification> {
           icon: Icon(Icons.arrow_back, color: Colors.black),
         ),
         actions: [
-          IconButton(
-            onPressed: () {
-            },
-            icon: Icon(Icons.home, color: Colors.black),
+          SizedBox(
+            child: TextButton(
+                onPressed: (){},
+                child: Text("전체 삭제", style: TextStyle(color: Colors.black, fontSize: 13),)
+            ),
           ),
           SizedBox(width: 10,)
         ],
       ),
-      body: Text("알림"),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _notificationList.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(width: 0.2, color: Colors.grey),
+                    ),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 15, top: 5),
+                                child: Icon(Icons.brush, size: 18, color: Color(0xFF556944),),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    width: MediaQuery.of(context).size.width * 0.75,
+                                    child: Text(
+                                      _notificationList[index]['content'],
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(_notificationList[index]['time'], style: TextStyle(color: Colors.grey),),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: SizedBox(
+                          width: 30,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Icon(Icons.clear, size: 19, color: Colors.black,),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10,)
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
