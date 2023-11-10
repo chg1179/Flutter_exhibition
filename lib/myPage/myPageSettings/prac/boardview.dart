d91005ee820e34ecf6e36e57fa7fbb69b107532a
Yerim <lim997@naver.com>
2023-11-10 16:00:17 +0900

    admin main delete

--- lib/myPage/myPageSettings/prac/boardview.dart
+++ lib/myPage/myPageSettings/prac/boardview.dart
@@ -1,24 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import '../../../admin/main.dart';
-import '../../../firebase_options.dart';
-
 //게시글 상세보기
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
-}
-
-
-
 class BoardView extends StatefulWidget {
   final DocumentSnapshot document;
   const BoardView({required this.document});
